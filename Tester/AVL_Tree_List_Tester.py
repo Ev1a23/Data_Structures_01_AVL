7309d10fdda63a655e6a6c7096df95ca8c625706
Eviatar Shemesh <eviatarshemesh23@gmail.com>
2022-03-22 23:43:43 +0200

    tester for GetRoot

--- Tester/AVL_Tree_List_Tester.py
+++ Tester/AVL_Tree_List_Tester.py
@@ -17,5 +17,21 @@ class Test_AVL_Tree_list(unittest.TestCase):
         tree.root = None
         self.assertEqual(True, tree.empty())
 
+    def test_GetRoot(self):
+        tree = AVLTreeList()
+        self.assertEqual(None, tree.getRoot())
+        root = AVLNode("3")
+        tree.root = root
+        self.assertEqual(root, tree.getRoot())
+        root.setValue("5")
+        self.assertEqual(root, tree.getRoot())
+        root2 = AVLNode("3")
+        tree.root = root2
+        self.assertEqual(root2, tree.getRoot())
+        #root2 = None
+        #self.assertEqual(None, tree.getRoot())
+
+
+
     if __name__ == "__main__":
         unittest.main()
